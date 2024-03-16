b550e3aec8d615581a12506bb4629d9167aad4bf
karlsoosalu <Karl.Soosalu@gmail.com>
2024-03-16 13:56:53 +0200

    Add swagger api

--- src/main/java/org/esi/grable/GrableApplication.java
+++ src/main/java/org/esi/grable/GrableApplication.java
@@ -1,13 +1,22 @@
 package org.esi.grable;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GrableApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrableApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GrableApplication.class, args);
+    }
 
+    @Bean
+    public OpenAPI grableAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Grable")
+                        .description("Grable - API documentation"));
+    }
 }
