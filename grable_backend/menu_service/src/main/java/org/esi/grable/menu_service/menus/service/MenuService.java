774c0f88496247698ad8dbc744651b90e39c3b78
karlsoosalu <Karl.Soosalu@gmail.com>
2024-04-26 15:59:53 +0300

    fix build 2

--- grable_backend/menu_service/src/main/java/org/esi/grable/menu_service/menus/service/MenuService.java
+++ grable_backend/menu_service/src/main/java/org/esi/grable/menu_service/menus/service/MenuService.java
@@ -2,16 +2,11 @@ package org.esi.grable.menu_service.menus.service;
 
 import org.esi.grable.menu_service.menus.model.Menu;
 import org.esi.grable.menu_service.menus.repository.MenuRepository;
-import org.esi.grable.restaurant_service.restaurants.model.Restaurant;
-import org.esi.grable.restaurant_service.restaurants.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
-
-import static java.util.Objects.nonNull;
 
 @Service
 public class MenuService {
@@ -19,26 +14,11 @@ public class MenuService {
     @Autowired
     private MenuRepository menuRepository;
 
-    @Autowired
-    private WebClient.Builder webClientBuilder;
-
     public Menu createMenu(Long restaurantId, LocalDateTime lastUpdated) {
-        Restaurant restaurant = webClientBuilder
-                .build()
-                .get()
-                .uri("http://localhost:8080/restaurants/{restaurantId}", restaurantId)
-                .retrieve()
-                .bodyToMono(Restaurant.class)
-                .block();
-
-        if (nonNull(restaurant)) {
-            Menu menu = new Menu();
-            menu.setLastUpdated(lastUpdated);
-            menu.setRestaurantId(restaurant.getId());
-            return menuRepository.save(menu);
-        } else {
-            throw new IllegalArgumentException("Restaurant with ID " + restaurantId + " not found");
-        }
+        Menu menu = new Menu();
+        menu.setLastUpdated(lastUpdated);
+        menu.setRestaurantId(restaurantId);
+        return menuRepository.save(menu);
     }
 
     public Menu getMenuByRestaurantId(Long restaurantId) {
